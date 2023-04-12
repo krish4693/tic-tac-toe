bce79c4db1e6e128e5a95ffa1580ce63a5c2d543
krish <krishnanandunnikrishnan@gmail.com>
2023-04-12 07:06:37 +0530

    lifting state

--- src/App.js
+++ src/App.js
@@ -5,34 +5,38 @@ import { useState } from 'react';
 
 
 
-function Square({ value ,onSquareClick}) {
+function Square({ value, onSquareClick }) {
   return <button className='square' onClick={onSquareClick}>{value}</button>
 }
 
 export default function Board() {
   const [square, setSquare] = useState(Array(9).fill(null))
 
+
+  function handleClick() {
+    const nextSquare = square.slice();
+    nextSquare[0] = "X";
+    setSquare(nextSquare);
+  }
+
   return (
     <div>
       <div className='board-row'>
-        <Square value={square[0]} />
-        <Square value={square[1]} />
-
-        <Square value={square[2]} />
+        <Square value={square[0]} onSquareClick={handleClick} />
+        <Square value={square[1]} onSquareClick={handleClick} />
+        <Square value={square[2]} onSquareClick={handleClick} />
 
       </div>
       <div className='board-row'>
         <Square value={square[3]} />
         <Square value={square[4]} />
-
         <Square value={square[5]} />
       </div>
       <div className='board-row'>
         <Square value={square[6]} />
         <Square value={square[7]} />
-
         <Square value={square[8]} />
       </div>
-  </div>
+    </div>
   );
 }
