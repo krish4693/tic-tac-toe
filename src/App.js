afea1ddd51b35bbaffb46fa7733bed641a19f22f
krish <krishnanandunnikrishnan@gmail.com>
2023-04-12 12:56:24 +0530

    if condition for checking winning in progress

--- src/App.js
+++ src/App.js
@@ -62,7 +62,11 @@ function calculateWinner(){
   ]
   for(let i=0;i<lines.length;i++){
     const [a,b,c]=lines[i]
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
     
   }
+  return null;
 
 }
