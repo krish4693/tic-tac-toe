be0ebe8c5b32e8c84d9d1e780d38ddf334bf4c4c
krish <krishnanandunnikrishnan@gmail.com>
2023-04-18 12:48:54 +0530

    added history state

--- src/App.js
+++ src/App.js
@@ -105,6 +105,7 @@ function Board() {
 export default function Game() {
   const [isNext,setIsNext]=useState(true)
   const [history,setHistory]=useState(Array(9).fill(null))
+  const currentSquare=history[history.length-1]
   return (
     <div className='game'>
       <div className='game-board'>
